a56c88bce911f38c33d4a27658e9b0efe0aff1ce
Matt Payne <Payne@MattPayne.org>
2025-01-18 13:17:48 -0600

    still broken -- bard is faililng me.

--- hono1/server.ts
+++ hono1/server.ts
@@ -1,14 +1,9 @@
 import { serve } from "https://deno.land/std@0.195.0/http/server.ts";
 import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { serveFile } from "https://deno.land/x/hono/middleware/file.ts"; 
 
 const app = new Hono();
-const dist = './angular1/dist/angular1/browser';
 
-app.get("*", async (c) => {
-  return c.send({
-    root: dist,
-    index: "index.html",
-  });
-});
+app.get("*", serveFile("./dist")); 
 
 serve(app.fetch.bind(app), { port: 8000 });
